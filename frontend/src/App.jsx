8a8aa46e8cd1c940fd430f6a3f93dbbcb050281a
thepekka007 <tintomt01@gmaiil.com>
2026-03-31 15:38:25 +0530

    dsfii

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import CheckoutPage from './pages/CheckoutPage';
