fbeadd5d4354efde551d7a64ab2fa4a471149dac
thepekka007 <tintomt01@gmaiil.com>
2026-05-23 15:46:37 +0530

    corrected

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    "https://yourfrontend.onrender.com",
+    "https://frontend.onrender.com",
     "http://localhost:5173",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
